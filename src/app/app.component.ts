38616a5a7ccd45110b1c1e49c0758b97a6b8f4b6
ltciro-yuxi <ltciro@gmail.com>
2018-04-18 17:06:40 -0500

    first play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  private playList: any[] = [
+    {name: 'Desaprendiendo', played: false},
+    {name: 'La vida plena', played: false}
+    ];
+    private songPlaying = '';
+    private isPlaying = false;
+
+  play(song: any) {
+     this.isPlaying = this.songPlaying === song.name ? !this.isPlaying : true;
+     this.songPlaying = song.name ;
+  }
 }
