d896d4e970d3a6aaa012ff3aeb694ee0d14b360b
ltciro-yuxi <ltciro@gmail.com>
2018-04-18 18:39:58 -0500

    print hooks in console

--- src/app/song/song.component.ts
+++ src/app/song/song.component.ts
@@ -1,20 +1,60 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges,
+  OnDestroy, DoCheck, AfterViewInit, AfterViewChecked, AfterContentInit,
+  AfterContentChecked
+} from '@angular/core';
 
 @Component({
   selector: 'app-song',
   templateUrl: './song.component.html',
   styleUrls: ['./song.component.css']
 })
-export class SongComponent implements OnInit {
+export class SongComponent implements OnInit, OnChanges, OnDestroy, DoCheck,
+ AfterViewInit, AfterViewChecked, AfterContentInit, AfterContentChecked {
   @Input() idx;
   @Input() song;
   @Output() play = new EventEmitter();
   constructor() { }
 
-  ngOnInit() {
-  }
   playSong() {
     this.play.emit(this.song);
+  }
+
+  /**====================
+   * HOOKS
+   ===========================**/
+  ngOnInit()    {  console.log(`onInit song ${this.song.name}`); }
+
+  ngOnDestroy() {  console.log(`onDestroy song ${this.song.name}`); }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // tslint:disable-next-line:forin
+    for (const propName in changes) {
+      const chng = changes[propName];
+      const cur  = JSON.stringify(chng.currentValue);
+      const prev = JSON.stringify(chng.previousValue);
+      console.log(` song ${this.song.name} ${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
+  }
+  ngDoCheck() { console.log(`ngDoCheck song ${this.song.name}`); }
+
+  ngAfterViewInit() {
+    // viewChild is set after the view has been initialized
+    console.log(`AfterViewInit song ${this.song.name}`);
+  }
+
+  ngAfterViewChecked() {
+    // viewChild is updated after the view has been checked
+    console.log(`ngAfterViewChecked song ${this.song.name}`);
+  }
+  ngAfterContentInit() {
+    // contentChild is set after the content has been initialized
+    console.log(`AfterContentInit song ${this.song.name}`);
+  }
+
+  ngAfterContentChecked() {
+    // contentChild is updated after the content has been checked
+    console.log(`AfterContentChecked song ${this.song.name}`);
   }
 
 }
--- src/app/state-player/state-player.component.ts
+++ src/app/state-player/state-player.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-state-player',
   templateUrl: './state-player.component.html',
   styleUrls: ['./state-player.component.css']
 })
-export class StatePlayerComponent implements OnInit {
+export class StatePlayerComponent {
   @Input() isPlaying;
   @Input() songPlaying;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  /**====================
+   * HOOKS
+   ===========================**/
+   ngOnInit()    {  console.log(`onInit state-player`); }
+
+   ngOnDestroy() {  console.log(`onDestroy state-player`); }
+ 
+   ngOnChanges(changes: SimpleChanges) {
+     console.log(changes, 'nOnChanges state-player');
+     // tslint:disable-next-line:forin
+     for (const propName in changes) {
+       const chng = changes[propName];
+       const cur  = JSON.stringify(chng.currentValue);
+       const prev = JSON.stringify(chng.previousValue);
+       console.log(`state-player ${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+     }
+   }
+   ngDoCheck() { console.log(`ngDoCheck state-player`); }
+ 
+   ngAfterViewInit() {
+     // viewChild is set after the view has been initialized
+     console.log('AfterViewInit state-player');
+   }
+ 
+   ngAfterViewChecked() {
+     // viewChild is updated after the view has been checked
+     console.log('ngAfterViewChecked state-player');
+   }
+   ngAfterContentInit() {
+     // contentChild is set after the content has been initialized
+     console.log('AfterContentInit');
+   }
+ 
+   ngAfterContentChecked() {
+     // contentChild is updated after the content has been checked
+     console.log('AfterContentChecked state-player');
+   }
+ 
 
 }
